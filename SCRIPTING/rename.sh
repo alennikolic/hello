fd0ee6e75d4da1a47f19c9e2d742c77cf44b3bfa
alennikolic <57729607+alennikolic@users.noreply.github.com>
2019-11-14 12:38:59 +0100

    Update rename.sh

--- SCRIPTING/rename.sh
+++ SCRIPTING/rename.sh
@@ -3,19 +3,32 @@
 
 FILES="/etc/*"
 STRING=".wav"
+STR1="audiofile_"
 var=0
 
 for file in $FILES
 do
     datestamp=`date '+%Y_%m_%d'`;
-    newfilename="audiofile_"$datestamp"_`printf %03d $var`"$STRING
-    if [ ! -f $newfilename ]; then
-    
-        if [[ "$file" == *"$STRING" ]]; then
 
-            echo "File "$file" is now: "$newfilename;
-            mv $file $newfilename
-            var=$((var+1))
-        fi
+    STR2="_`printf %03d $var`"
+    newfilename="$STR1$datestamp$STR2$STRING"
+
+    if [[ "$file" == *"$STR1"* ]]
+    then
+        echo "File $file content $STR1"
+
+    elif [[ -f $newfilename ]] 
+    then
+        echo "File $file alredy exist"
+ 
+    elif [[ "$file" == *"$STRING" ]] 
+    then
+
+       	 	echo "File "$file" is now: "$newfilename
+        	mv $file $newfilename
+        	var=$((var+1))
+    else
+	echo "Nothing has changed for $file" 
     fi
+
 done
